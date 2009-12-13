0f582b108ebc1859d3cb714a430ab137d7c7cf3b
Lenni <leonard.ehrenfried@web.de>
2009-12-13 16:55:39 +0100

    basic functionality finished

--- gmailer.py
+++ gmailer.py
@@ -1,74 +1,69 @@
 #!/usr/bin/python
 # encoding: utf-8
-import smtplib, sys, MimeWriter, StringIO, base64, mimetypes, datetime
+import smtplib, sys, datetime, os
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
+from optparse import OptionParser
 
-print sys.argv
-
-
-files=sys.argv
-files=files[1:]
 
 class Email():
+    sender=None
+    recipient=None
+    
+    subject_line="[Gmailer] Automatic backup"
+    body="This is an automated backup sent on %s." % str(datetime.date.today())
+    attachments=[]
+    
+    # obviously, do override these properties
+    smtp_user=None
+    smtp_pass=None
+    
+    #override these values if you want to use another service
+    smtp_server="smtp.gmail.com"
+    smtp_port=587
+    
     """docstring for ClassName"""
     def __init__(self):
-        self.sender=None
-        self.recipient=None
-        
-        self.subject_line="[Gmailer] automatic backup"
-        self.body="This is an automated backup sent on %s" % str(datetime.date.today())
-        self.attachments=[]
-        
-        #override these values if you want to use another service
-        self.smtp_server="smtp.gmail.com"
-        self.smtp_port=587
-        
-        self.smtp_user=None
-        self.smtp_pass=None
+        pass
     
     def construct_message(self):
-        message = StringIO.StringIO()
-        writer = MimeWriter.MimeWriter(message)
-        writer.addheader('Subject', self.subject_line)
-        writer.startmultipartbody('mixed')
+        msg = MIMEMultipart()
 
-        # start off with a text/plain part
-        part = writer.nextpart()
-        body = part.startbody('text/plain')
-        body.write(text)
+        msg['From'] = self.sender
+        msg['To'] = self.recipient
+        msg['Subject'] = self.subject_line
 
-        # now add an attachment
-        for i in self.attachments:
-			part = writer.nextpart()
-			part.addheader('Content-Transfer-Encoding', 'base64')
-			part.addheader('Content-disposition:', 'attachment; filename="'+i+'"')
-			mtype=mimetypes.guess_type(i)
-			mtype=mtype[0]
-			if i.endswith('gpg')==True:
-				mtype='application/pgp'			
-			
-			
-			print "added attachment: "+i+" ("+mtype+")"
-			body = part.startbody(mtype)
-			base64.encode(open(i, 'rb'), body)
-
-        # finish off
-        writer.lastpart()
+        msg.attach(MIMEText(self.body))
         
-        return writer.getvalue()
-    
+        for i in self.attachments:
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload(open(i, 'rb').read())
+            Encoders.encode_base64(part)
+            part.add_header('Content-Disposition',
+                    'attachment; filename="%s"' % os.path.basename(i))
+            msg.attach(part)
+        
+        return msg.as_string()
+        
     def check_all_arguments(self):
-        REQUIRED=[this.sender, this.recipent, this.smtp_user, this.smtp_pass]
+        REQUIRED=[self.smtp_user, self.smtp_pass]
+        
+        if self.sender==None:
+            self.sender="%s@gmail.com"%self.smtp_user
+            self.recipient=self.sender
         
         for i in REQUIRED:
             if (i==None):
                 print "You have not set all the properties required for an email."
-                print "Please set sender, recipient, stmp_user and smtp_pass and try again."
+                print "Please set smtp_user and smtp_pass and try again."
                 return False
         return True
     
     def send(self):
-        if (check_all_arguments()):
-            message=this.construct_message()
+        if (self.check_all_arguments()):
+            message=self.construct_message()
             # send the mail
             
             mailServer = smtplib.SMTP(self.smtp_server, self.smtp_port)
@@ -76,14 +71,31 @@ class Email():
             mailServer.starttls()
             mailServer.ehlo()
             mailServer.login(self.smtp_user, self.smtp_pass)
-            mailServer.sendmail(gmail_user, to, msg.as_string())
+            mailServer.sendmail(self.sender, self.recipient, message)
             # Should be mailServer.quit(), but that crashes...
             mailServer.close()
 
 def main():
     """docstring for main"""
+    
+    parser = OptionParser()
+    parser.add_option("-a", "--attachment", dest="attachment",
+                      help="Add an attachment to the email", metavar="FILE")
+    
+    parser.add_option("-u", "--user", dest="user",
+                      help="SMTP username (most likely your GMail username)", metavar="USER")
+                      
+    parser.add_option("-p", "--pass", dest="passwd",
+                      help="SMTP password (most likely your GMail password)", metavar="PASS")
+    
+    (options, args) = parser.parse_args()
+    
     mail=Email()
+    mail.smtp_user=options.user
+    mail.smtp_pass=options.passwd
+    mail.attachments.append(options.attachment)
+    mail.send()
+    print "Email sent to %s" % mail.recipient
 
 if __name__ == '__main__':
     main()
-
